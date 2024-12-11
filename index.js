6bca64623d46cf08ab62598a72589ae4f1d51f34
Ernestas Ilgevicius <ernestas963@yahoo.com>
2024-12-11 21:13:06 +0200

    Added script with linked api

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelector("Form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.querySelector("#userName").value;
+
+  if (name) {
+    const data = { Name: name };
+    fetch("https://testapi.io/api/Hvrny/resource/UsersList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector("#responseMessage").innerText =
+          "Name submitted successfully!";
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        document.querySelector("#responseMessage").innerText =
+          "Error submitting name!";
+        console.error("Error:", error);
+      });
+  } else {
+    document.querySelector("#responseMessage").innerText =
+      "Please enter a name.";
+  }
+});
